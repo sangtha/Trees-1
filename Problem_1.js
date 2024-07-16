63016ccc618f6072ee742c37a069919372543b28
Sangeetha Chandrashekar <ch.sangtha@gmail.com>
2024-07-16 19:54:48 -0400

    Trees-1 completed

---
+++ Problem_1.js
@@ -0,0 +1,65 @@
+// Time Complexity : O(n) // n is number of tree nodes
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+// Solution using inorder traversal since it produces a sorted tree items
+// var isValidBST = function(root) {
+//     var prev = null;
+//     var flag = true; 
+
+
+//     var inorderBST = function(root){
+//         if(!flag) return;
+//         if(root == null) return;
+//         inorderBST(root.left);
+//         if(prev!= null && prev.val >= root.val){
+//             flag = false;
+//         }
+//         prev = root;
+//         inorderBST(root.right);
+//     }
+
+//     inorderBST(root);
+//     return flag;
+// };
+// Can do this using global flag i.e. void based recursion or booleab based i.e. where each recursion fn returns boolean value
+//Also works with any order traversals i.e pre, in or post. 
+var isValidBST = function(root) {
+    var flag = true;
+
+    var isValidBSTHelper = (root, min, max) => {
+            //base case
+            if(root === null) return;
+            if(!flag) return; // conditional recursion
+            if (min!= null && root.val <= min){
+                flag = false;
+            }
+            if(max!=null && root.val >= max){
+                flag = false; 
+            }
+
+            //logic
+            isValidBSTHelper(root.left, min , root.val);
+
+            isValidBSTHelper(root.right, root.val, max);
+    }
+    isValidBSTHelper(root, null, null);
+    return flag;
+ }
+
---
+++ Problem_2.js
@@ -0,0 +1,39 @@
+// Time Complexity : O(n) // n is number of tree nodes
+// Space Complexity : O(n) // extra space taken by slicing the inorder and preorder arrays for left and right children traversal
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+
+//Below is one way of solving using extra space; Can be done using pointers too without using slice
+var buildTree = function(preorder, inorder) {
+    //base
+    if(preorder.length === 0) return null;
+
+    //recursive logic
+    let rootVal = preorder[0];
+    let rootIndex = inorder.indexOf(rootVal); // O(n) search
+    let root = new TreeNode(rootVal);
+
+    let inorderLeft = inorder.slice(0, rootIndex); // from 0 to not including the rootIndex element
+    let inorderRight = inorder.slice(rootIndex+1); // from not including the rootIndex to end of the array
+    let preorderLeft = preorder.slice(1, inorderLeft.length+1);
+    let preorderRight = preorder.slice(inorderLeft.length+1, preorder.length);
+    
+    root.left = buildTree(preorderLeft, inorderLeft);
+    root.right = buildTree(preorderRight, inorderRight);
+
+    return root;
+};
